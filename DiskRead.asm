dccb51dd6d22c79a302affcc0ddb50efaa06081e
beaterblank <mohanteja.g2019@vitstudent.ac.in>
2022-01-24 21:37:44 +0530

    disk read

---
+++ DiskRead.asm
@@ -0,0 +1,18 @@
+PROGRAM_SPACE equ 0x7e00
+READ_DISK:
+	mov bx,PROGRAM_SPACE 
+	mov ah,2
+	mov al,4   ;number of sectors to read
+	mov dl,[BOOT_DISK] ;disk
+	mov ch,0x00 ;cylynder number
+	mov dh,0x00 ;head number
+	mov cl,0x02 ;sector number
+	int 0x13
+	jnc  exit
+	mov bx,DISK_READ_ERR_STR
+	call print
+exit:ret
+BOOT_DISK:db 0
+DISK_READ_ERR_STR:db "DISK READ Failed...",0
+
+
--- boot.asm
+++ boot.asm
@@ -1,11 +1,19 @@
 ;code part :
 
+mov [BOOT_DISK],dl
+
+
 [org 0x7c00]   ;offest for memory
+mov bp,0x7c00  ;set stack pointer
+mov sp,bp
 
-mov bx,buffer  ;get str pointer into bx
-call scan      ;scan and st
 
-mov bx,buffer  ;get str pointer into bx
+call READ_DISK
+
+mov bx,PROGRAM_SPACE  ;get str pointer into bx
+call scan      ;scan and store
+
+mov bx,PROGRAM_SPACE  ;get str pointer into bx
 call print		;print
 
 
@@ -15,6 +23,7 @@ jmp $          ;run forever do nothing
 
 %include "print.asm"
 %include "scan.asm"
+%include "DiskRead.asm"
 
 ;memory part :
 
@@ -22,3 +31,7 @@ buffer:db 0           ;defining a pointer to a memory
  
 times 510-($-$$) db 0 ;the boot loader should be 512 bytes in size
 db 0x55,0xaa   		 ;ending with 0x55aa
+
+times 2048 db 0      ;filling one sector with M
+
+
